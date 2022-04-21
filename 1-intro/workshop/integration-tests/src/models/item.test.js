d4335c167fc370de8b95630be2a0a9a0a3f98d03
Leveque <levequetom77@gmail.com>
2022-04-21 15:36:39 +0200

    Add 4 others IntegrationTest for Item

--- 1-intro/workshop/integration-tests/src/models/item.test.js
+++ 1-intro/workshop/integration-tests/src/models/item.test.js
@@ -6,6 +6,9 @@ mongoose.connect('mongodb://mongo:27017/docker-node-mongo-test', { useNewUrlPars
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
+beforeEach(async () => {
+    await Item.deleteMany();
+});
 
 it('should create an item', () => {
     const item = new Item({
@@ -22,4 +25,46 @@ it('should save an item', async () => {
 
     const NewItem = await Item.findOne({ name: 'My Item' });
     expect(item.name).toBe(NewItem.name);
+});
+
+it('should find all items', async () => {
+    const item = new Item({
+        name: 'My Item'
+    });
+    await item.save();
+
+    const items = await Item.find({});
+    expect(items.length).toBe(1);
+});
+
+it('should delete an item', async () => {
+    const item = new Item({
+        name: 'My Item'
+    });
+    await item.save();
+
+    await Item.deleteOne({ name: 'My Item' });
+    const items = await Item.find({});
+    expect(items.length).toBe(0);
+});
+
+it('should update an item', async () => {
+    const item = new Item({
+        name: 'My Item'
+    });
+    await item.save();
+
+    await Item.updateOne({ name: 'My Item' }, { name: 'My Item Updated' });
+    const items = await Item.find({});
+    expect(items[0].name).toBe('My Item Updated');
+});
+
+it('should find an item', async () => {
+    const item = new Item({
+        name: 'My Item'
+    });
+    await item.save();
+
+    const items = await Item.find({ name: 'My Item' });
+    expect(items[0].name).toBe('My Item');
 });
