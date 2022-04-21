e029085977fd1006fd46c607708855e93cca416a
Leveque <levequetom77@gmail.com>
2022-04-21 11:58:07 +0200

    First WorkShop E2E Finish

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -1,5 +1,13 @@
 Feature('Ynov Nantes');
 
-Scenario('Test Ynov Nantes Land Page', ({ I }) => {
+Scenario('Test Ynov Nantes Land Page', async ({ I }) => {
     I.amOnPage('https://www.ynov-nantes.com/');
+    I.click('.search-cta a.cta');
+
+    I.see('Que recherchez-vous ?','div');
+    I.fillField('input.searchfield', 'info');
+
+    await I.grabTextFrom('.results-list .search-preview-cursus');
+    I.see('Bachelor Informatique','.results-list .search-preview-cursus .preview-title .tile-title')
+
 });
