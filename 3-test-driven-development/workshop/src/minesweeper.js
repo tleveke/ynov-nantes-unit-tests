886ce0109cc8bd2effdaa3fc69a49a8ba47c1d93
Leveque <levequetom77@gmail.com>
2022-04-28 10:37:09 +0200

    init: Create Board

--- 3-test-driven-development/workshop/src/minesweeper.js
+++ 3-test-driven-development/workshop/src/minesweeper.js
@@ -1,1 +1,17 @@
- 
+const createBoard = (rows, columns) => {
+    const board = [];
+    //Colums
+    for (let i = 0; i < columns; i++) {
+        board.push([]);
+        //Rows
+        for (let j = 0; j < rows; j++) {
+            //Cell
+            board[i].push(".");
+        }
+    }
+    return board;
+};
+
+module.exports = {
+    createBoard,
+}; 
--- 3-test-driven-development/workshop/test/minesweeper.test.js
+++ 3-test-driven-development/workshop/test/minesweeper.test.js
@@ -1,2 +1,40 @@
-describe("MineSweeper", function() {
+const { createBoard } = require("../src/minesweeper");
+
+const inputInitData = [
+    {
+        rows: 4,
+        columns: 3,
+    },
+    {
+        rows: 3,
+        columns: 4,
+    },
+    {
+        rows: 2,
+        columns: 2,
+    },
+    {
+        rows: 1,
+        columns: 1,
+    },
+];
+
+describe("MineSweeper", function () {
+
+    describe.each(inputInitData)("Create minesweeper board", (data) => {
+        test(`MineSweeper Board should have ${data.rows} rows and ${data.columns} columns`, () => {
+            const board = createBoard(data.rows, data.columns);
+            //The board should have length equal to the number of columns
+            expect(board.length).toBe(data.columns);
+            board.forEach((row) => {
+                //Each row should have length equal to the number of rows
+                expect(row.length).toBe(data.rows);
+                for (const cell of row) {
+                    //Each cell should be a string
+                    expect(cell).not.toBe(null);
+                }
+            });
+        });
+    });
+
 });
