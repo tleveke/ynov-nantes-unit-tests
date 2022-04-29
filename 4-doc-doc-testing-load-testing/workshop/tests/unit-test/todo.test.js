51ce32c0052165748d30bad0665d5389e437aa3c
Leveque <levequetom77@gmail.com>
2022-04-29 11:59:04 +0200

    test: unit test

--- 4-doc-doc-testing-load-testing/workshop/tests/unit-test/todo.test.js
+++ 4-doc-doc-testing-load-testing/workshop/tests/unit-test/todo.test.js
@@ -1,6 +1,27 @@
-describe('true', () => {
-    it('should be true', () => {
-        expect(true).toBe(true);
+const ToDo = require('../../toDoModel').ToDo;
+
+describe('ToDo Model', () => {
+    it('should create', () => {
+        const todo = new ToDo({
+            text: 'Test todo'
+        });
+        expect(todo.text).toBe('Test todo');
+        expect(todo.done).toBe(false);
     });
-    }
-);
+    it('should update name', async () => {
+        const todo = new ToDo({
+            text: 'Test todo'
+        });
+        todo.text = 'Test todo updated';
+        
+        expect(todo.text).toBe('Test todo updated');
+    });
+    it('should update done', async () => {
+        const todo = new ToDo({
+            text: 'Test todo'
+        });
+        todo.done = true;
+        
+        expect(todo.done).toBe(true);
+    });
+});
